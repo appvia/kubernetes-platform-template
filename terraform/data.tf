bf7b8f9cbfcf12d97d52d158d148fab4dbd19f04
Rohith Jayawardene <gambol99@gmail.com>
2026-04-03 12:17:00 +0100

    chore: updated the makefile for the terraform code

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,6 +3,3 @@ data "aws_caller_identity" "current" {}
 
 ## Find the current region
 data "aws_region" "current" {}
-
-## Find the current canonical user id
-data "aws_canonical_user_id" "current" {}
